d598d57f6f9f011b7ec02f6b4797e2b3cc4d4367
okorkov <alex.okorkov@gmail.com>
2021-02-09 12:47:01 -0800

    commit tree dd3550fbe051059dd9154a867cd90469d50ee7eb

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+  return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  return drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  return drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  return drivers.shift()
+}
+
+function appendDriver(name) {
+  let result = drivers.slice();
+  result.push(name);
+  return result;
+}
+
+function prependDriver(name) {
+  let result = drivers.slice();
+  result.unshift(name);
+  return result;
+}
+
+function removeLastDriver() {
+  let result = drivers.slice();
+  result.pop();
+  return result;
+}
+
+function removeFirstDriver() {
+  let result = drivers.slice();
+  result.shift();
+  return result;
+}
